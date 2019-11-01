8ffacd0517247fe0a8a013d6fd461c51a3c78403
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2019-11-01 13:24:41 +0200

    Simple redux reducer with action generators and redux store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,16 +1,37 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }, action) => {
+// Action Generators
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({ 
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({ decrementBy = 10 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const setCount = ({ count = 101 } = {}) => ({
+    type: 'SET',
+    count
+});
+
+const resetCount = () => ({ type: 'RESET'});
+
+// Reducers
+// 1. Reducers are pure functions.
+// 2. Never change state or action.
+
+const countReducer = (state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 10;
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             };
         case 'RESET':
             return {
@@ -23,41 +44,23 @@ const store = createStore((state = { count: 0 }, action) => {
         default:
             return state;
     }
-    console.log('running');
-    return state;
-});
+};
+
+const store = createStore(countReducer);
 
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
 });
 
-// Actions
-// Increment the count
-// Set the count to 0
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-store.dispatch({
-    type: 'INCREMENT',
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-    type: 'DECREMENT',
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 10
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-    type: 'RESET'
-})
+store.dispatch(resetCount());
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-})
+store.dispatch(setCount({ count: -101 }));
